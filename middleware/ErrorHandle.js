16c795f8fd4cf2891d67940314998234991fdc0e
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-27 10:21:12 +0200

    Middleware added

---
+++ middleware/ErrorHandle.js
@@ -0,0 +1,22 @@
+function errHandling(err, req, res, next) {
+    if (err || res.statusCode >= 400) {
+     res.json (
+   
+       {
+           status:err.status ||
+           res.statusCode || 500,
+           err: "An error has occured please contact administrator"
+       }
+     )
+   }
+   next()
+   
+   
+   
+   }
+   export {
+   
+   errHandling
+   
+   }
+   
