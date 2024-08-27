1e59db8b90c346b35a315c3457155150341730c1
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-27 22:10:30 +0200

    navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="wrapper">
+<NavbarComp/>
+<main class="container-fluid">
+<router-view/>
+
+</main>
+
+</div>
 </template>
+<script>
+import NavbarComp from '@/components/NavbarComp.vue'
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+export default {
+components: {
 
-nav {
-  padding: 30px;
-}
+  NavbarComp
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+
 }
+</script>
+<style src="./assets/Styles/Style.css">
+
 </style>
---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid d-flex justify-content-start align-items-center">
+        <a class="navbar-brand Logo" href="#">Home Of The Broken Heroes</a>
+        
+
+        <input type="text" placeholder="Search for comrades..." class="form-control SearchBar" id="Searcher">
+        <div class="spreader d-flex justify-content-around mt-2">
+        <i class="bi bi-house-door-fill IconColour h4 ml-3"></i>
+        <i class="bi bi-envelope-fill IconColour h4 ml-3"></i>
+        <i class="bi bi-bell-fill  IconColour h4"></i>
+        <i class="bi bi-person-fill IconColour h4"></i>
+
+    </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+    
+  </template>
+  
