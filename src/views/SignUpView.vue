63ad98b497ac8281a55ea5583397f90d5659fe4d
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-28 13:58:59 +0200

    manflag

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="container-fluid">
+    <div class="imgContainer">
+      <h2 class="centered">Our History</h2>
+      <h4 class="centered2">We didn’t start out as a website. After WW2. A group of veterans. Beaten and broken by the war went home not to praise but to disdain and resentment.</h4> 
+      <h4 class="centered3">They met up every week with other veterans. Sending letters. Having group support meetings. Then the internet was born...</h4> 
+    <img src="https://richtershotline.github.io/Pimages/Images/America.jpg" id="America" loading="lazy" class="img-fluid">
+  </div>
+
+</div>
+
+</template>
