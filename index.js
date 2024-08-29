b57b82a18ba6f504d8565976360ceec48693f7a2
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-29 14:27:48 +0200

    added cors

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 //// 
+import cors from "cors"
 import {PostersRouter, express} from "./controller/UsersController.js"
 import {MessageRouter} from "./controller/PostController.js"
 import path from "path"//create express app
 const app = express()
 const port = +process.env.PORT || 4000// Middleware
 // creating an express app
-app.use((req, res, next) => {
-
-res.header("Access-Control-Allow-Origin", "*");
-next()
-
-})
-
 app.use(
     
     express.static("./static"),
@@ -20,6 +14,21 @@ app.use(
         extended: true
 
     }))
+app.use((req, res, next) => {
+
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Credentials", "true");
+res.header("Access-Control-Allow-Methods", "*");
+res.header("Access-Control-Request-Methods", "*");
+res.header("Access-Control-Allow-Headers", "*");
+res.header("Access-Control-Expose-Headers", "Authorization");
+
+next()
+
+})
+
+
 // THIS IS THE ACTUAL ENDPOINT THAT THE USER WILL NEED TO ENTER TO GET TO RELEVANT PAGES
 app.use("/users", PostersRouter)
 app.use("/posts", MessageRouter)
