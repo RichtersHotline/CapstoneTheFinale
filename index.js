a0a5a191e492a18016e49b32fe88960a84cfc27c
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-30 12:04:13 +0200

    removal of duplicate

--- index.js
+++ index.js
@@ -16,19 +16,15 @@ app.use(
 
     }))
 
-app.use((req, res, next) => {
-
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Credentials", "true");
-res.header("Access-Control-Allow-Methods", "*");
-res.header("Access-Control-Request-Methods", "*");
-res.header("Access-Control-Allow-Headers", "*");
-res.header("Access-Control-Expose-Headers", "Authorization");
-
-next()
-
-})
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Access-Control-Allow-Methods", "*");
+        res.header("Access-Control-Request-Methods", "*");
+        res.header("Access-Control-Allow-Headers", "*");
+        res.header("Access-Control-Expose-Headers", "Authorization");
+        next()
+    })
 
 
 
