cdbdba0634ae3f30b8f1eb697d4646bdf983f38a
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-09-03 15:49:33 +0200

    added a replies modal and controller

--- model/index.js
+++ model/index.js
@@ -1,12 +1,15 @@
 import { Posts } from "./Posts.js";
 import { Users } from "./Users.js";
-
+import {Reply} from "./Replies.js"
 const Messages = new Posts()
 
 const Posters = new Users()
 
+const Replies = new Reply()
+
 export {
 
 Messages,
-Posters
+Posters,
+Replies
 }
