2b4ab06ffa86be02b58cb82cd1730529de831bb9
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-09-09 16:25:41 +0200

    added footer

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,17 @@
 <router-view/>
 
 </main>
-
+<FooterComp v-if="!$route.meta.hideFooter"/>
 </div>
 </template>
 <script>
 import NavbarComp from '@/components/NavbarComp.vue'
-
+import FooterComp from '@/components/FooterComp.vue';
 export default {
 components: {
 
-  NavbarComp
+  NavbarComp,
+  FooterComp
 
   }
 
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer class="container-fluid footerstyle">
+  <div class="footerCon">
+  <p class="text-left me-1">HOTBH &copy; <span>{{cYear}}</span> </p>  
+  <router-link class="me-1 txt" to="/about">About</router-link>
+  <router-link class="me-1 txt" to="/contact">Contact</router-link>
+  <router-link class="me-1 txt" to="/signup">signup</router-link>
+  <router-link class="me-1 txt" to="/profile">profile</router-link>  </div>
+  
+    </footer>
+  
+  </template>
+  
+  <script>
+  export default {
+  data() {
+  
+      return {
+    cYear: new Date().getUTCFullYear()
+  
+  
+      }
+  
+  
+  
+  },
+  
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
